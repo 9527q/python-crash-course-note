e4150e6779f0b5417e11395b45fd7a7d0634fd56
IkeLiu <liuyu_qi617@163.com>
2020-09-03 20:40:17 +0800

    修正代码格式

--- code/guess_number.py
+++ code/guess_number.py
@@ -7,9 +7,10 @@ import random
 def guess_game():
     msg_start = '我想好了一个正整数！猜猜它是几：'
     msg_gt = '哥，大了大了！！再猜一次吧：'
-    msg_lt = "哎，小了，小了~~再给你一次机会："
-    msg_eq = "猜对了，你是我肚子里的蛔虫吗？但是没有奖励哦~~\n要不要再玩一次?（“好”/“不”）："
-    msg_thx = "欢迎下次再来哦~~"
+    msg_lt = '哎，小了，小了~~再给你一次机会：'
+    msg_eq = '猜对了，你是我肚子里的蛔虫吗？但是没有奖励哦~~'\
+        '\n要不要再玩一次?（“好”/“不”）：'
+    msg_thx = '欢迎下次再来哦~~'
     num = random.randint(1, 100)
     guess_num = int(input(msg_start))
     while 1:
